c859b484d839e4c070ab1857383e9003838321e6
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2024-05-05 14:30:43 +0530

    day-3

--- users/views.py
+++ users/views.py
@@ -9,5 +9,9 @@ def profiles(request):
 
 def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
-    context = {'profile':profile}
+
+    topskills = profile.skill_set.exclude(description__exact="")
+    otherskills = profile.skill_set.filter(description="")
+
+    context = {'profile':profile, 'topskills':topskills, 'otherskills':otherskills}
     return render(request, 'users/user-profile.html', context)
