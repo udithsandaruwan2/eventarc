9a47794385b07a17bc9e0343a45962aed8f19bba
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2024-07-09 03:42:28 +0530

    day-08.1
    modifications with thoughts section

--- thoughts/models.py
+++ thoughts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import Profile
+import uuid
 
-# Create your models here.
+class Thought(models.Model):
+    author = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    content = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.content)
--- thoughts/views.py
+++ thoughts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Thought
 
 def thoughts(request):
-    # profile = request.user.profile
-    # context = {'profile':profile}
-    return render(request, 'thoughts/thoughts.html')
+    if request.user.is_authenticated:
+        profile = request.user.profile
+    else:
+        profile = None
+    thoughts = Thought.objects.all()
+    context = {'profile':profile, 'thoughts':thoughts}
+    return render(request, 'thoughts/thoughts.html', context)
