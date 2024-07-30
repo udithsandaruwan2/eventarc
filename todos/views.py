3869e89c3b06782be5790bc5476b3c0406f012a8
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2024-07-30 16:34:48 +0530

    day11.2

--- todos/views.py
+++ todos/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 def todoList(request):
     page = 'todos'
+    
+    if request.method == 'POST':
+        return redir
     context = {'page':page}
     return render(request, 'todos/todos.html', context)
